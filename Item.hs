84c9267eb9d0b4e1e8d65ad1fbe517d04552b448
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-03-11 14:48:09 -0800

    Begin work

---
+++ Item.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE OverloadedStrings         #-}
+
+module Item where
+
+import Data.Monoid
+import Data.Proxy
+import Data.Text (Text)
+import Data.Typeable
+import Test.QuickCheck
+
+import qualified Data.Text as T
+
+
+data Item = forall a. Typeable a => Item
+  { itemName       :: Text
+  , itemImports    :: [(Text, [Text])]
+  , itemType       :: Proxy a
+  , itemCheck      :: a -> IO Bool
+  , itemIsOperator :: Bool
+  }
+
+
+importsToText :: [(Text, [Text])] -> Text
+importsToText =
+  T.unlines . map (\(m, fs) -> "import " <> m <> "(" <> T.intercalate "," fs <> ")")
+
+
+qcCheck1
+  :: (Show a, Arbitrary a, Eq b)
+  => (a -> b)
+  -> (a -> b)
+  -> IO Bool
+qcCheck1 f g = do
+  result <- quickCheckResult (\a -> f a == g a)
+  case result of
+    Success _ _ _ -> pure True
+    _ -> pure False
+
+qcCheck2
+  :: (Show a, Show b, Arbitrary a, Arbitrary b, Eq c)
+  => (a -> b -> c)
+  -> (a -> b -> c)
+  -> IO Bool
+qcCheck2 f g = do
+  result <- quickCheckResult (\a b -> f a b == g a b)
+  case result of
+    Success _ _ _ -> pure True
+    _ -> pure False
+
+
+appendItem :: Item
+appendItem =
+  Item
+  "++"
+  []
+  (Proxy :: Proxy ([Int] -> [Int] -> [Int]))
+  (qcCheck2 (++))
+  True
--- Main.hs
+++ Main.hs
@@ -1,4 +1,57 @@
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Item
+
+import Data.Monoid
+import Data.Text     (Text)
+import Data.Typeable (Typeable)
+import Data.Proxy
+import Language.Haskell.Interpreter
+import System.FilePath.Posix
+import System.IO
+import System.IO.Temp
+
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
+
 main :: IO ()
-main = undefined
+main = go appendItem
+
+go :: Item -> IO ()
+go (Item name imports (p :: Proxy t) check is_operator) = do
+  T.putStrLn name
+  body <-
+    let go acc = do
+          T.getLine >>= \case
+            "EOF" -> pure (T.unlines (reverse acc))
+            xs    -> go (xs:acc)
+    in go []
+
+  (path, h) <- openTempFile "temp" "M.hs"
+  let mname = takeBaseName path
+
+  T.hPutStrLn h $ T.unlines
+    [ "{-# LANGUAGE NoImplicitPrelude #-}"
+    , "{-# LANGUAGE Safe #-}"
+    , "module " <> T.pack mname <> " where"
+    , importsToText imports
+    , "data " <> T.pack mname -- this prevents imports in the body
+    , body
+    ]
+  hClose h
+
+  result <- runInterpreter $ do
+    set [searchPath := ["temp"]]
+    loadModules [mname]
+    setImportsQ [("Prelude", Nothing), (mname, Just "M")]
+    interpret ("(M." ++ T.unpack name ++ ")") (as :: t)
+
+  case result of
+    Left err -> print err
+    Right f -> do
+      check f
+      pure ()
